a7688fae2bf8af8cddbdb470534ea03750b7b35d
anassstya <408183@edu.itmo.ru>
2024-05-12 19:47:36 +0300

    d

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-const menuBurger = document.querySelector('.header__burger-menu');
-const popup = document.querySelector('.popup');
-const body = document.querySelector('body');
+//const menuBurger = document.querySelector('.header__burger-menu');
+//const popup = document.querySelector('.popup');
+//const body = document.querySelector('body');
 
-menuBurger.addEventListener('click', () => {
-  popup.classList.add('popup__active');
-  menuBurger.classList.add('burger__hide'); 
-  body.classList.add('fade'); 
-});
+//menuBurger.addEventListener('click', () => {
+//  popup.classList.add('popup__active');
+ // menuBurger.classList.add('burger__hide'); 
+//});
 
-const popupClose = document.querySelector('.popup__close');
+//const popupClose = document.querySelector('.popup__close');
 
-popupClose.addEventListener('click', () => {
-  popup.classList.remove('popup__active');
-  menuBurger.classList.remove('burger__hide'); 
-  body.classList.remove('fade'); 
-});
+//popupClose.addEventListener('click', () => {
+ // popup.classList.remove('popup__active');
+//  menuBurger.classList.remove('burger__hide'); 
+//});
+//
