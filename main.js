4e4d316fc87d09ca43927f1fb3ba33e72482c72d
anassstya <408183@edu.itmo.ru>
2024-05-12 19:23:16 +0300

    s

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const body = document.querySelector('body');
 
 menuBurger.addEventListener('click', () => {
   popup.classList.add('popup__active');
-  menuBurger.classList.add('burger__hide'); // Скрыть бургер-меню при открытии попапа
-  body.classList.add('fade'); // Применить затемнение фона body
+  menuBurger.classList.add('burger__hide'); 
+  body.classList.add('fade'); 
 });
 
 const popupClose = document.querySelector('.popup__close');
 
 popupClose.addEventListener('click', () => {
   popup.classList.remove('popup__active');
-  menuBurger.classList.remove('burger__hide'); // Показать бургер-меню при закрытии попапа
-  body.classList.remove('fade'); // Убрать затемнение фона body
+  menuBurger.classList.remove('burger__hide'); 
+  body.classList.remove('fade'); 
 });
